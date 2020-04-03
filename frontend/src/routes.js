53bb63d6d438b06efeb02618c069664d4b49b97b
João Luiz Antunes Neto <netoanttunes@gmail.com>
2020-04-03 04:17:31 -0300

    Construção do Front-end

---
+++ frontend/src/routes.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+
+import cadastro from './Pages/cadastro';
+import lista from './Pages/lista';
+import mapa from './Pages/mapa';
+
+
+export default function Routes() {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={cadastro} />
+                <Route path="/lista" component={lista} />
+                <Route path="/mapa" component={mapa} />
+                 
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
